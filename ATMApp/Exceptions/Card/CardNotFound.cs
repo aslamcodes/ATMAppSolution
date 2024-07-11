2f0aa1d8f0161229da0e91c2b33c96a081a7f324
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-07-11 14:36:48 +0530

    Add Accout and Card Repository

---
+++ ATMApp/Exceptions/Card/CardNotFound.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ATMApp.Exceptions.Card
+{
+    [Serializable]
+    internal class CardNotFound : Exception
+    {
+        public CardNotFound()
+        {
+        }
+
+        public CardNotFound(string? message) : base(message)
+        {
+        }
+
+        public CardNotFound(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CardNotFound(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- ATMApp/Models/Card.cs
+++ ATMApp/Models/Card.cs
@@ -6,8 +6,6 @@
         public string CardNumber { get; set; }
         public string Pin { get; set; }
         public string PinHashKey { get; set; }
-        public decimal Balance { get; set; }
-
         public int AccountId { get; set; }
 
         public Account Account { get; set; }
