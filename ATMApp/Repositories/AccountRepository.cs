14c43d807a3462e451636e379bb203d7fe5f4af7
unknown <ahamedhuzair13@gmail.com>
2024-07-11 12:54:03 +0530

    Repo Added

---
+++ ATMApp/Repositories/AccountRepository.cs
@@ -0,0 +1,33 @@
+﻿using ATMApp.Interfaces;
+using ATMApp.Models;
+
+namespace ATMApp.Repositories
+{
+    public class AccountRepository : IRepository<int, Account>
+    {
+        public Task<Account> Add(Account item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Account>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> Update(Account item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ATMApp/Repositories/CardRepository.cs
@@ -0,0 +1,33 @@
+﻿using ATMApp.Interfaces;
+using ATMApp.Models;
+
+namespace ATMApp.Repositories
+{
+    public class CardRepository : IRepository<int, Card>
+    {
+        public Task<Card> Add(Card item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Card> Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Card> Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Card>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Card> Update(Card item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
